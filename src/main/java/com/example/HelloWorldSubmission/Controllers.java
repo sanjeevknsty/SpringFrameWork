cda9094d1e7f749f1f21af85cb303e503ba72004
sanjeevknsty <ksanjeev0359@gmail.com>
2025-05-19 17:36:51 +0530

    post commit

--- src/main/java/com/example/HelloWorldSubmission/Controllers.java
+++ src/main/java/com/example/HelloWorldSubmission/Controllers.java
@@ -3,10 +3,14 @@ package com.example.HelloWorldSubmission;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import jakarta.annotation.PostConstruct;
 
 @RestController
 @RequestMapping("/hello")
@@ -25,5 +29,10 @@ public class Controllers {
 	@GetMapping("/param/{name}")
 	public String sayHello(@PathVariable String name) {
 		return "Hello" + name;
+	}
+	
+	@PostMapping("/post")
+	public String sayHello(@RequestBody User user) {
+		return "Hello " + user.getfirstName() + " " + user.getLastName();
 	}
 }
---
+++ src/main/java/com/example/HelloWorldSubmission/User.java
@@ -0,0 +1,21 @@
+package com.example.HelloWorldSubmission;
+
+public class User {
+	private String firstName;
+	private String lastName;
+	
+	public String getfirstName() {
+		return this.firstName;
+	}
+	public void setFirstName(String name) {
+		this.firstName = name;
+	}
+	
+	public String getLastName() {
+		return this.lastName;
+	}
+	public void setLastName() {
+		this.lastName = lastName;
+	}
+	
+}
