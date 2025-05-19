1883b2b4ab267970d834c4fed1c181cbc732ed19
sanjeevknsty <ksanjeev0359@gmail.com>
2025-05-19 17:18:42 +0530

    hello commit

---
+++ src/main/java/com/example/HelloWorldSubmission/Controllers.java
@@ -0,0 +1,16 @@
+package com.example.HelloWorldSubmission;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/hello")
+public class Controllers {
+
+	@RequestMapping(value= {"","/",})
+	public String sayHello() {
+		return "Hello from BridgeLabz";
+	}
+}
---
+++ src/main/java/com/example/HelloWorldSubmission/HelloWordSubmissionApplication.java
@@ -0,0 +1,14 @@
+package com.example.HelloWorldSubmission;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HelloWordSubmissionApplication {
+	
+
+	public static void main(String[] args) {
+		SpringApplication.run(HelloWordSubmissionApplication.class, args);
+	}
+
+}
