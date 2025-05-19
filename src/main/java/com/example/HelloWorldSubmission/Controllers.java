f5ae7ddcb250e379697bc278c978342674f41fc5
sanjeevknsty <ksanjeev0359@gmail.com>
2025-05-19 17:26:10 +0530

    query commit

--- src/main/java/com/example/HelloWorldSubmission/Controllers.java
+++ src/main/java/com/example/HelloWorldSubmission/Controllers.java
@@ -2,7 +2,10 @@ package com.example.HelloWorldSubmission;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -12,5 +15,10 @@ public class Controllers {
 	@RequestMapping(value= {"","/",})
 	public String sayHello() {
 		return "Hello from BridgeLabz";
+	}
+	
+	@RequestMapping(value= {"/query"},method = RequestMethod.GET)
+	public String queryWithName(@RequestParam(value = "name") String name) {
+		return "Hello " + name;
 	}
 }
